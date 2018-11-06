25f65d9714a616c75348d9c6db94559b2821e337
Zsmith21 <32179413+Zsmith21@users.noreply.github.com>
2018-11-06 11:49:22 -0500

    The beginning of my project! Woop woop

---
+++ src/main/java/edu/springframework/zdppetclinic/ZdpPetClinicApplication.java
@@ -0,0 +1,12 @@
+package edu.springframework.zdppetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZdpPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZdpPetClinicApplication.class, args);
+    }
+}
