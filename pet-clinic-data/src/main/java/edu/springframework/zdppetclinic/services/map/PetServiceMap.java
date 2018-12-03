375ef323534b929fe1ff516bc722ff54bba74766
Zsmith21 <zsmith21@github.com>
2018-12-03 00:55:06 -0500

    implemented map based services. closes #6

---
+++ pet-clinic-data/src/main/java/edu/springframework/zdppetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,35 @@
+package edu.springframework.zdppetclinic.services.map;
+
+import edu.springframework.zdppetclinic.model.Owner;
+import edu.springframework.zdppetclinic.model.Pet;
+import edu.springframework.zdppetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+}
---
+++ pet-clinic-data/src/main/java/edu/springframework/zdppetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,36 @@
+package edu.springframework.zdppetclinic.services.map;
+
+import edu.springframework.zdppetclinic.model.Owner;
+import edu.springframework.zdppetclinic.model.Pet;
+import edu.springframework.zdppetclinic.model.Vet;
+import edu.springframework.zdppetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+}
