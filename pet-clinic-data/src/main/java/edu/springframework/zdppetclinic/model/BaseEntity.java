57afa6e2885d64486444a81b87674024f07bcee1
Zsmith21 <zsmith21@github.com>
2018-12-28 18:14:39 -0500

    Added list of Owners to Index Page. Closes #16

--- pet-clinic-data/src/main/java/edu/springframework/zdppetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/edu/springframework/zdppetclinic/model/BaseEntity.java
@@ -1,10 +1,12 @@
 package edu.springframework.zdppetclinic.model;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.io.Serializable;
 
+@Entity
 public class BaseEntity implements Serializable {
 
     @Id
